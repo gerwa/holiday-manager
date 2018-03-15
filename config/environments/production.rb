970e8cbfb0ffd8f7ef0d189cc1f11a9c327d39ed
gerwa <gertraud.wachmann@hotmail.ch>
2018-03-15 21:39:09 +0100

    raise deliv errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   #config.action_mailer.default_url_options = { :host => 'https://sleepy-everglades-88866.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
